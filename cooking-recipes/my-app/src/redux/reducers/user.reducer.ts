a3dc54c3ead50b8258b0e433281bf6d4259521bc
AmirAdarGit <amiradar12345@gmail.com>
2023-05-01 12:29:14 +0300

    first commit

--- cooking-recipes/my-app/src/redux/reducers/user.reducer.ts
+++ cooking-recipes/my-app/src/redux/reducers/user.reducer.ts
@@ -7,7 +7,6 @@ const initialState = {
   RecipesDBCollectionId: '',
   userDBCollectionId: '',
 };
-console.log("amirr")
 function userReducer(state = initialState, action: any) {
   switch (action.type) {
     case 'LOGIN':
